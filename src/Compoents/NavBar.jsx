1b5fc7ed44cf1c2278744be6c427924c662817b4
Fernando <fernandodarder01@gmail.com>
2022-03-29 22:08:54 -0300

    Correccion de className

--- src/Compoents/NavBar.jsx
+++ src/Compoents/NavBar.jsx
@@ -5,30 +5,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default function NavBar(){
     return(
         <>
-            <div class="container-fluid row bg-dark " >
-                <h1 class="bg-dark text-white">Prode oficial</h1>
-                    <ul class="nav justify-content-center flex-row flex-md-row">
-                        <li class="d-inline-block"><a class="navbar-brand text-white " href="#">
+            <div className="container-fluid row bg-dark " >
+                <h1 className="bg-dark text-white">Prode oficial</h1>
+                    <ul className="nav justify-content-center flex-row flex-md-row">
+                        <li className="d-inline-block"><a className="navbar-brand text-white " href="#">
                             Inicio
                             </a>
                         </li>
-                        <li class="d-inline-block"><a class="navbar-brand text-white " href="#">
+                        <li className="d-inline-block"><a className="navbar-brand text-white " href="#">
                             Mis pronosticos
                             </a>
                         </li>
-                        <li class="d-inline-block"><a class="navbar-brand text-white " href="#">
+                        <li className="d-inline-block"><a className="navbar-brand text-white " href="#">
                             Torneo de amigos
                             </a>
                         </li>
-                        <li class="d-inline-block"><a class="navbar-brand text-white " href="#">
+                        <li className="d-inline-block"><a className="navbar-brand text-white " href="#">
                             Mi candidato
                             </a>
                         </li>
-                        <li class="d-inline-block"><a class="navbar-brand text-white " href="#">
+                        <li className="d-inline-block"><a className="navbar-brand text-white " href="#">
                             Grupos
                             </a>
                         </li>
-                        <li class="d-inline-block"><a class="navbar-brand text-white " href="#">
+                        <li className="d-inline-block"><a className="navbar-brand text-white " href="#">
                             Fixture
                             </a>
                         </li>
